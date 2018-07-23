fc7ed2871c5442c29628c6bdbd31fa4a8deffe6e
Mazin Ahmed <mazen160@users.noreply.github.com>
2018-07-23 15:11:11 +0300

    Update emojis-webshell.php

--- Proof-of-Concepts/emojis-webshell/emojis-webshell.php
+++ Proof-of-Concepts/emojis-webshell/emojis-webshell.php
@@ -5,7 +5,7 @@
  * A Proof of Concept for using Emojis in PHP.
  * Author: Mazin Ahmed <Mazin AT MazinAhmed DOT net>
  * Homepage: https://github.com/mazen160/public/tree/master/Proof-of-Concepts/emojis-webshell
- * License: The MIT License (MIT) - https://github.com/mazen160/public/tree/master/Proof-of-Concepts/emojis-webshell/lisense.md
+ * License: The MIT License (MIT) - https://github.com/mazen160/public/blob/master/Proof-of-Concepts/emojis-webshell/LICENSE.txt
  *
  * Legal Disclaimer:
  * This project is made for educational and ethical testing purposes only. Usage of project for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.
